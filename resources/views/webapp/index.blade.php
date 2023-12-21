80926b655aa5bde45d54d5b18bb35e52d93c795e
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-12-21 14:55:51 +0200

    add audio for index

--- resources/views/webapp/index.blade.php
+++ resources/views/webapp/index.blade.php
@@ -1,1 +1,18 @@
-index page
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Playback</title>
+</head>
+
+<body>
+    <h1>Playback</h1>
+
+    <audio controls>
+        <source src="{{ asset('storage/sounds/short_forest.mp3') }}" type="audio/mpeg">
+    </audio>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SoundController;
 
 Route::get('/', function () {
     return view('webapp.index');
 });
+
+Route::get('/playback', [SoundController::class, 'playback']);
