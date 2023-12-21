89abe40053e1a4c90a558700a2d845eafa0904da
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-12-21 18:55:07 +0200

    add app, header, add style cdn

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <!-- Логотип -->
+        <a class="navbar-brand" href="{{ route('index') }}">
+            SoundHub360
+        </a>
+
+    </div>
+</nav>
--- resources/views/webapp/index.blade.php
+++ resources/views/webapp/index.blade.php
@@ -1,26 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Playback</title>
-</head>
+@section('content')
 
-<body>
-    <h1>Playback</h1>
-    <table class="table table-bordered">
-        @foreach ($sounds as $sound)
-        <tr>
-            <td>{{ $sound->name }}</td>
-            <td>
-                <audio controls>
-                    <source src="{{ asset('storage/' . $sound->path) }}" type="audio/mpeg">
-                </audio>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-</body>
+<h1>Playback</h1>
+<table class="table table-bordered">
+    @foreach ($sounds as $sound)
+    <tr>
+        <td>{{ $sound->name }}</td>
+        <td>
+            <audio controls>
+                <source src="{{ asset('storage/' . $sound->path) }}" type="audio/mpeg">
+            </audio>
+        </td>
+    </tr>
+    @endforeach
+</table>
 
-</html>
+@endsection
